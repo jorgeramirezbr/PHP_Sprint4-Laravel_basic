8678caa879bce16da3c3eaac1be0baba8c94521e
jorge <jorge.ramirez.br@gmail.com>
2023-10-10 14:08:44 +0200

    partidos relationships

--- LigaMVC/app/Models/Partido.php
+++ LigaMVC/app/Models/Partido.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Partido extends Model
 {
     use HasFactory;
+
+    public function equipoLocal()
+    {
+        return $this->belongsTo(Equipo::class, 'equipo_local');
+    }
+
+    public function equipoVisitante()
+    {
+        return $this->belongsTo(Equipo::class, 'equipo_visitante');
+    }
 }
