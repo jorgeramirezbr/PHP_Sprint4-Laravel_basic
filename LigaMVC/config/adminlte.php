0c806189a83885faa2b12af7442dd21873ce8afb
jorge <jorge.ramirez.br@gmail.com>
2023-10-22 20:52:03 +0200

    Equipos table

--- LigaMVC/config/adminlte.php
+++ LigaMVC/config/adminlte.php
@@ -315,14 +315,14 @@ return [
             'text'        => 'Equipos',
             'url'         => 'equipos',
             'icon'        => 'far fa-fw fa-file',
-            'label'       => 'CRUD',
+            'label'       => 'Indice',
             'label_color' => 'warning',
         ],
         [
             'text'        => 'Partidos',
             'url'         => 'partidos',
             'icon'        => 'far fa-fw fa-file',
-            'label'       => 'CRUD',
+            'label'       => 'Indice',
             'label_color' => 'warning',
         ],
         ['header' => 'account_settings'],
@@ -330,66 +330,7 @@ return [
             'text' => 'profile',
             'url'  => 'user/profile',
             'icon' => 'fas fa-fw fa-user',
-        ],
-        [
-            'text' => 'change_password',
-            'url'  => 'admin/settings',
-            'icon' => 'fas fa-fw fa-lock',
-        ],
-        [
-            'text'    => 'multilevel',
-            'icon'    => 'fas fa-fw fa-share',
-            'submenu' => [
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-                [
-                    'text'    => 'level_one',
-                    'url'     => '#',
-                    'submenu' => [
-                        [
-                            'text' => 'level_two',
-                            'url'  => '#',
-                        ],
-                        [
-                            'text'    => 'level_two',
-                            'url'     => '#',
-                            'submenu' => [
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                                [
-                                    'text' => 'level_three',
-                                    'url'  => '#',
-                                ],
-                            ],
-                        ],
-                    ],
-                ],
-                [
-                    'text' => 'level_one',
-                    'url'  => '#',
-                ],
-            ],
-        ],
-        ['header' => 'labels'],
-        [
-            'text'       => 'important',
-            'icon_color' => 'red',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'warning',
-            'icon_color' => 'yellow',
-            'url'        => '#',
-        ],
-        [
-            'text'       => 'information',
-            'icon_color' => 'cyan',
-            'url'        => '#',
-        ],
+        ]
     ],
 
     /*
--- LigaMVC/resources/views/equipos/index.blade.php
+++ LigaMVC/resources/views/equipos/index.blade.php
@@ -8,25 +8,45 @@
 @stop
 
 @section('content')
-    <div class="flex justify-center">
-        <p class="my-8">
-            <a class="bg-blue-700 hover:bg-pink-700 text-white font-bold px-3 py-1 rounded m-5 text-center" href="{{route('home')}}">Inicio</a>
-        </p>
-        <p class="my-8">
-            <a class="bg-blue-700 hover:bg-pink-700 text-white font-bold px-3 py-1 rounded m-5 text-center" href="{{route('equipos.create')}}">Crear equipo</a>
-        </p>
+    <div>
+        <div class="flex justify-center">
+            <p class="my-8">
+                <a class="bg-blue-700 hover:bg-pink-700 text-white font-bold px-3 py-1 rounded m-5 text-center" href="{{route('home')}}">Inicio</a>
+            </p>
+            <p class="my-8">
+                <a class="bg-blue-700 hover:bg-pink-700 text-white font-bold px-3 py-1 rounded m-5 text-center" href="{{route('equipos.create')}}">Crear equipo</a>
+            </p>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Equipo</th>
+                            <th>Puntos</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($equipos as $equipo)
+                            <tr>
+                                <td>{{$equipo->id}}</td>
+                                <td><a class= " text-blue-600" href="{{route('equipos.show', $equipo->id)}}">{{$equipo->name}}</a></td>
+                                <td>{{$equipo->puntos}}</td>
+                                <td width="10px">
+                                    <a href="{{route('equipos.edit', $equipo)}}" class="btn btn-primary">Editar</a>
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+            <div class="card-footer">
+                {{$equipos->links()}}
+            </div>
+        </div>
     </div>
-    <div class="container">
-        <ol class="list-disc list-inside">
-            @foreach ($equipos as $equipo)
-                <li>
-                    <a class= " text-blue-600" href="{{route('equipos.show', $equipo->id)}}">{{$equipo->name}}</a> -
-                    {{$equipo->puntos}} puntos
-                </li>
-            @endforeach
-        </ol>
-    </div>
-    {{$equipos->links()}}
 @stop
 
 @section('css')
@@ -36,4 +56,5 @@
 @section('js')
     <script> console.log('Hi!'); </script>
     <script src="https://cdn.tailwindcss.com"></script>
+    @livewireScripts
 @stop
